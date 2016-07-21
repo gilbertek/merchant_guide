cf65de96c3e470e15791722ae4605110c0172f0a
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2016-07-21 12:56:15 -0400

    updates

--- lib/information_builder.rb
+++ lib/information_builder.rb
@@ -1,16 +1,35 @@
 class InformationBuilder
-  attr_reader :question
+  attr_reader :question, :db
 
-  INFORMATION_QUESTION_PATTERN = /^how much is\s+(.+)[ ?]$/.freeze
+  UNIT_INFORMATION_PATTERN = /^(\w*) is (\w*)$/.freeze
+  MINERAL_INFORMATION_PATTERN = /(\w+) ? (\w+) ? (\w+) is (\d+) Credits$/.freeze
 
   def initialize(question)
     @question = question
+    @db ||= {}
+    parse
   end
 
-  def value
+  def parse
+    parse_unit if question =~ UNIT_INFORMATION_PATTERN
+    parse_mineral if question =~ MINERAL_INFORMATION_PATTERN
+  end
+
+  def parse_unit
+    data = question.match(UNIT_INFORMATION_PATTERN)
+    db[data[1]] = data[2]
+  end
+
+  def parse_mineral
+    data = question.match(MINERAL_INFORMATION_PATTERN)
+
   end
 
   def self.is_mineral_info? question
-    question =~ INFORMATION_QUESTION_PATTERN
+    question =~ UNIT_INFORMATION_PATTERN || question =~ MINERAL_INFORMATION_PATTERN
+  end
+
+  def valid_info
+
   end
 end
